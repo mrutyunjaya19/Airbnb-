2fa5e024cbff012863007ecb1e740c8da0b0d9ad
Mrutyunjaya Mishra <mrutyunjayamishra293@gmail.com>
2025-07-03 20:10:00 +0530

    add Project Files

---
+++ models/review.js
@@ -0,0 +1,29 @@
+// const { string, types } = require("joi");
+const mongoose = require("mongoose");
+const { authorize } = require("passport");
+ const Schema = mongoose.Schema;
+
+ const reviewSchema = new Schema ({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    listing: {
+    type: Schema.Types.ObjectId,
+    ref: "Listing",
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref:"User",
+  },
+ });
+
+
+ module.exports = mongoose.model("Review", reviewSchema);
---
+++ models/reviewSchema.js
@@ -0,0 +1,15 @@
+// const { Schema } = require("./review");
+
+// const reviewSchema = new Schema({
+//     Comment: String,
+//     rating: {
+//         type: Number,
+//         min: 1,
+//         max: 5,
+//     },
+//     createAt: {
+//         type: Date,
+//         default: Date.now(),
+//     },
+// });
+// exports.reviewSchema = reviewSchema;
