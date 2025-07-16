b02895c9ab1847af6b9cb01d10c200e53e255a8e
Mrutyunjaya Mishra <mrutyunjayamishra293@gmail.com>
2025-07-16 14:51:11 +0530

    commited

--- app.js
+++ app.js
@@ -126,7 +126,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+    console.log("server is listening to port 8080");
 });
-
-
